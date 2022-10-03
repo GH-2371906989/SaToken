04e342d52912ab448550386da3752f62bde4ffe6
guhuan <2371906989@qq.com>
2022-10-03 10:35:06 +0800

    默认的

---
+++ src/main/java/com/gu/satokenitem/service/JotterService.java
@@ -0,0 +1,4 @@
+package com.gu.satokenitem.service;
+
+public interface JotterService {
+}
---
+++ src/main/java/com/gu/satokenitem/service/impl/JotterServiceImpl.java
@@ -0,0 +1,12 @@
+package com.gu.satokenitem.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gu.satokenitem.mapper.JotterMapper;
+import com.gu.satokenitem.pojo.Jotter;
+import com.gu.satokenitem.service.JotterService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JotterServiceImpl extends ServiceImpl<JotterMapper, Jotter> implements JotterService {
+
+}
